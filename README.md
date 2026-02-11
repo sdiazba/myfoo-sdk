131b7ec899ecb2222f93a5feebc62dde21a945c1
Silviano Diaz <sdiazba@dhcp-10-157-69-155.mx.oracle.com>
2026-02-11 11:52:40 -0600

    First version of MyFoo SDK

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Foo SDK
+The most minimal SDK you'll ever find.
+
+## Installation
+```bash
+pip install myfoo-sdk
---
+++ src/myfoo/__init__.py
@@ -0,0 +1,6 @@
+def hello_myfoo():
+    """Returns a simple greeting from the SDK."""
+    return "Hello from the My Foo SDK!"
+
+# This ensures that 'hello_myfoo' is easily accessible
+__all__ = ["hello_myfoo"]
